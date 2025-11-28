676256b058fcdb0bfce018de1f97fa6e616d458e
Divanchik <mashohasergej@gmail.com>
2025-11-28 22:10:12 +0300

    it works

--- BuisnessBot/TelegramBot/handlers/start.py
+++ BuisnessBot/TelegramBot/handlers/start.py
@@ -3,6 +3,9 @@ from aiogram.filters import CommandStart, Command
 from aiogram.types import Message
 import ollama
 from decouple import config
+import textwrap
+
+
 
 help = open('C:/Users/mm1/Desktop/Buisness Chat Bot/BuisnessChatBot/BuisnessBot/TelegramBot/handlers/txts/help.txt', 'r')
 model = config('MODEL')
@@ -14,20 +17,21 @@ def get_resp(message):
     response = ollama.chat(
         model = model,
         messages=[
-            {'role': 'system', 'content': 'Ты - помощник в крупной IT-компании. К тебе могут обращаться как сотрудники компании, так и ее клиенты. Твоя задача - давать понятные всем инструкции по решению их проблем'},
+            {'role': 'system', 'content': 'Ты - помощник в крупной IT-компании. К тебе могут обращаться как сотрудники компании, так и ее клиенты. Твоя задача - давать понятные всем инструкции по решению их проблем, используй сдержанный язык - без восклицаний, только официально-деловой стиль'},
             {'role': 'user', 'content': message}
         ],
         stream=False
     )
-    print(f'response was gotten \n {response}')
-    return str(response['message']['content'])
+
+    answer = response['message']['content']
+    return answer
 
 
 @start_router.message(CommandStart())
 async def start(message: Message):
     await message.answer('Здравствуйте, бот готов к работе. Можете задавать свой вопрос.')
 
-@start_router.message(F.text == '/help')
+@start_router.message(Command('/help'))
 async def help(message: Message):
     await message.answer(str(help.readlines()))
     
@@ -35,6 +39,14 @@ async def help(message: Message):
 async def question(message: Message):
     try:
         await message.answer('Ответ подготавливается...')
-        await message.answer(get_resp(message.text))
-    except:
+
+        answer = get_resp(message.text)
+        
+        answers = textwrap.wrap(answer, 4096)
+
+        for i in answers:
+            await message.answer(i)
+
+    except Exception as error:
+       print(f'error occured while sending message {error}')
        await message.answer('Во время получения ответа произошла ошибка, повторите попытку позже.')
